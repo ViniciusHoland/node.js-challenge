69e9f06831040ad38151d6ff752243f3b483bc8c
ViniciusDev <viniciusdev10@gmail.com>
2025-02-14 10:06:58 -0300

    created router Transactions

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import transactionRouter from './routes/transactionsRouter.js'
 
 
 const app = express()
 app.use(express.json())
 
-const PORT = process.env.PORT ||3020 
+const PORT = process.env.PORT || 3020 
+
+app.use('/', transactionRouter)
 
 
 app.listen(PORT, () =>{
